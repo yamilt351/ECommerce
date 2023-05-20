d935947eec5345e42a74c9e5220f7a585a623839
yamil351 <leokasper@live.com.ar>
2023-05-20 12:39:28 -0300

    new navbar component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Constainer = styled.div`
-  height: 60px;
+  height: 4rem;
   width: 100%;
   background-color: red;
 `;
 const Wrapper = styled.div`
-  padding: 10px 20px;
+  padding: 1rem 2rem;
   display: flex;
   max-width: 1200px;
-  margin: auto;
   justify-content: space-between;
+  align-items: center;
+
 `;
 const Left = styled.div`
+  display: flex;
+  align-items: center;
   flex: 1;
 `;
+const Logo= styled.h1`
+  font-size: 2rem;
+  font-weight: bold;
+
+`
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 2rem;
+`;
+const Input = styled.input`
+  border: none;
+`;
+
 const Language = styled.span`
-  font-size: 14px;
+  font-size: 1.2rem;
   cursor: pointer;
 `;
 const Center = styled.div`
@@ -33,9 +51,13 @@ const Navbar = () => {
       <Wrapper>
         <Left>
           <Language>EN</Language>
-          <SearchContainer>search</SearchContainer>
+          <SearchContainer>
+            <Input></Input>
+          </SearchContainer>
         </Left>
-        <Center>Center</Center>
+        <Center>
+          <Logo>Yamil</Logo>
+        </Center>
         <Right>Right</Right>
       </Wrapper>
     </Constainer>
