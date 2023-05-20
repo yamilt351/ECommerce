91351cb29814997b2b964439fcc307d39fa3d2b3
yamil351 <leokasper@live.com.ar>
2023-05-20 10:07:00 -0300

    creating-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from './Components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-    
+        <Navbar/>
       </div>
     );
   }
